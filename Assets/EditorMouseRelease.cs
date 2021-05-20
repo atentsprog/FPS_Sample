064e8ae92374f983aa756ed0f01c66fa3945fd5b
chanseon park <hktrollgames@gmail.com>
2021-05-20 11:54:02 +0900

    tab키 눌러서 카메라 락 푸는 스크립트 추가

---
+++ Assets/EditorMouseRelease.cs
@@ -0,0 +1,40 @@
+﻿using FPSControllerLPFP;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorMouseRelease : MonoBehaviour
+{
+    FpsControllerLPFP controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<FpsControllerLPFP>();
+    }
+
+
+    bool mouseLock = true;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleMouseRelease();
+        }
+    }
+
+    private void ToggleMouseRelease()
+    {
+        mouseLock = !mouseLock;
+        SetMouseReleaseMode();
+    }
+
+    private void SetMouseReleaseMode()
+    {
+        controller.enabled = mouseLock;
+        if (mouseLock)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
