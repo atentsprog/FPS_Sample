186553b656b84df8e9e69bf4a154de91fdfef9d7
chanseon park <hktrollgames@gmail.com>
2021-05-20 12:10:45 +0900

    적이 플레이어를 추격 하는 기본 AI추가

---
+++ Assets/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public NavMeshAgent agent;
+    IEnumerator Start()
+    {
+        while(true)
+        { 
+            agent.destination = target.transform.position;
+            yield return null;
+        }
+    }
+}
