f573c25b2ee69b64b2c84498c1c61e943e962bec
chanseon park <hktrollgames@gmail.com>
2021-05-21 13:29:19 +0900

    공격 범위 확인하는 선이 잘못 그려지는 버그 수정

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -44,8 +44,8 @@ public class EditorGUI_Enemy : Editor
         item.viewingDistance = (float)Handles.ScaleValueHandle(item.viewingDistance, tr.position + tr.forward * item.viewingDistance, tr.rotation, 1, Handles.ConeHandleCap, 1);
 
         // 아크의 왼쪽 오른쪽 직선 그리기
-        Handles.DrawLine(tr.position, tr.forward.AngleToYDirection(-halfAngle) * item.viewingDistance);     // 왼쪽선 그리기.
-        Handles.DrawLine(tr.position, tr.forward.AngleToYDirection(halfAngle) * item.viewingDistance);      // 오른쪽선 그리기.
+        Handles.DrawLine(tr.position, tr.position + tr.forward.AngleToYDirection(-halfAngle) * item.viewingDistance);     // 왼쪽선 그리기.
+        Handles.DrawLine(tr.position, tr.position + tr.forward.AngleToYDirection(halfAngle) * item.viewingDistance);      // 오른쪽선 그리기.
     }
 }
 #endif
