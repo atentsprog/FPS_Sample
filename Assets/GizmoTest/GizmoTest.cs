2f0274064461acc19fed9b73cd20263e823fdcd1
hktrollgames <hktrollgames@gmail.com>
2021-05-21 18:14:28 +0900

    에디터 디버그용 문자열, 도형 그리는 함수 추가

--- Assets/GizmoTest/GizmoTest.cs
+++ Assets/GizmoTest/GizmoTest.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class GizmoTest : MonoBehaviour
 {
+    public float distance = 1;
+    public bool DrawSphere = true;
+    public bool DrawWireSphere = true;
+    public bool DrawWireCube = true;
+    public bool DrawRay = true;
+    public bool DrawIcon = true;
+    public bool DrawFrustum = true;
+
+    //private void OnDrawGizmosSelected() // 선택되었을때 표시하는 기즈모
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, 1);
+
+        if (DrawSphere)
+            Gizmos.DrawSphere(transform.position, distance);
+
+        if (DrawWireSphere)
+            Gizmos.DrawWireSphere(transform.position, distance);
+
+        //Vector3.one==new Vector3(1, 1, 1)
+        if (DrawWireCube)
+            Gizmos.DrawWireCube(transform.position, Vector3.one * distance);
+
+        if (DrawRay)
+            Gizmos.DrawRay(transform.position, transform.forward);
+
+        if (DrawIcon)
+            Gizmos.DrawIcon(transform.position, "testIcon.png");
+
+        if (DrawFrustum)
+            Gizmos.DrawFrustum(transform.position, 60, distance, 1, 1.5f);
+                                            // 위치, 각도(fov), 최대거리, 최소 거리, 
     }
 }
